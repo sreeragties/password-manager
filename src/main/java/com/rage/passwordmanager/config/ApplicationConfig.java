bdf3a37365484b7baff563a7275746c0dd7c8770
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2023-11-02 01:59:36 +0530

    feat: implemented JWT authentication filter

---
+++ src/main/java/com/rage/passwordmanager/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.rage.passwordmanager.config;
+
+import com.rage.passwordmanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
