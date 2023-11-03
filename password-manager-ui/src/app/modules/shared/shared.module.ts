2ed3400f31ca55e03d69ca7e0bd1042c495e0f1d
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2023-11-03 17:28:10 +0530

    feat: installed font-awesome

--- password-manager-ui/src/app/modules/shared/shared.module.ts
+++ password-manager-ui/src/app/modules/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   exports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ]
 })
 export class SharedModule { }
